bfcaeb0290c9ba2dfa65f8b7bad2ee452c09a920
孙路 <sunlu@sunludeMac.local>
2015-08-15 02:47:41 +0800

    add

---
+++ Abs/Abs/Util/ViewUtil.m
@@ -0,0 +1,42 @@
+//
+//  NSObject+ViewUtil.m
+//  Letsgo
+//
+//  Created by 孙路 on 15/8/13.
+//  Copyright (c) 2015年 Miku. All rights reserved.
+//
+
+#import "ViewUtil.h"
+
+@implementation ViewUtil : NSObject
+
++ (void) alert:(NSString*)message cancelButtonTitle:(NSString*) title{
+    UIAlertView *alertView =[[UIAlertView alloc] initWithTitle:nil message:message delegate:nil cancelButtonTitle:title otherButtonTitles:nil];
+    [alertView show];
+    [alertView release];
+}
+
++ (void) alert:(NSString*)message{
+    [ViewUtil alert:message cancelButtonTitle:@"关闭"];
+}
+
++ (void)loadDocument:(UIWebView *)webView url:(NSString *)url{
+    NSURLRequest *request;
+    if ([url hasPrefix:@"http://"]){
+        
+        request=[NSURLRequest requestWithURL:[NSURL URLWithString:url]];
+        
+    } else {
+        NSString *mainBundleDirectory=[[NSBundle mainBundle] bundlePath];
+        NSString *path=[mainBundleDirectory stringByAppendingPathComponent:url];
+        NSURL *url=[NSURL fileURLWithPath:path];
+        
+        
+        request=[NSURLRequest requestWithURL:url];
+    }
+    webView.scalesPageToFit=YES;
+    
+    [webView loadRequest:request];
+}
+
+@end
