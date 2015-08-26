6677dfd0d567bdbb54df61b6a9f22c962cb43dcf
sl <sorrymeika@163.com>
2015-08-26 09:15:00 -0700

    fx

--- Abs/Abs/Views/ViewController.mm
+++ Abs/Abs/Views/ViewController.mm
@@ -10,8 +10,13 @@
 #import "HybridView.h"
 #import "ViewUtil.h"
 #import "CPModalWebViewController.h"
+#import <CoreLocation/CoreLocation.h>
 
-@interface ViewController ()
+@interface ViewController ()<CLLocationManagerDelegate>{
+    
+    CLLocationManager *_locationManager;
+    NSString *_locationGettingCallback;
+}
 @end
 
 @implementation ViewController
@@ -61,6 +66,45 @@
     
     [ViewUtil loadDocument:hybridView url:@"index.html"];
     //[ViewUtil loadDocument:hybridView url:@"http://192.168.0.104:5559/"];
+}
+
+-(void) startGettingLocation{
+    //<--定位管理器
+    _locationManager=[[CLLocationManager alloc]init];
+    
+    if (![CLLocationManager locationServicesEnabled]) {
+        NSLog(@"定位服务当前可能尚未打开，请设置打开！");
+        return;
+    }
+    
+    //如果没有授权则请求用户授权
+    if ([CLLocationManager authorizationStatus]==kCLAuthorizationStatusNotDetermined){
+        [_locationManager requestWhenInUseAuthorization];
+    }else if([CLLocationManager authorizationStatus]==kCLAuthorizationStatusAuthorizedWhenInUse){
+        //设置代理
+        _locationManager.delegate=self;
+        //设置定位精度
+        _locationManager.desiredAccuracy=kCLLocationAccuracyBest;
+        //定位频率,每隔多少米定位一次
+        CLLocationDistance distance=10.0;//十米定位一次
+        _locationManager.distanceFilter=distance;
+        //启动跟踪定位
+        [_locationManager startUpdatingLocation];
+    }
+    //定位管理器-->
+}
+
+#pragma mark - CoreLocation 代理
+#pragma mark 跟踪定位代理方法，每次位置发生变化即会执行（只要定位到相应位置）
+//可以通过模拟器设置一个虚拟位置，否则在模拟器中无法调用此方法
+-(void)locationManager:(CLLocationManager *)manager didUpdateLocations:(NSArray *)locations{
+    CLLocation *location=[locations firstObject];//取出第一个位置
+    CLLocationCoordinate2D coordinate=location.coordinate;//位置坐标
+    NSLog(@"经度：%f,纬度：%f,海拔：%f,航向：%f,行走速度：%f",coordinate.longitude,coordinate.latitude,location.altitude,location.course,location.speed);
+    //如果不需要实时定位，使用完即使关闭定位服务
+    [_locationManager stopUpdatingLocation];
+    
+    [self hybridCallback:_locationGettingCallback params:[NSString stringWithFormat:@"%f,%f,%f,%f,%f",coordinate.longitude,coordinate.latitude,location.altitude,location.course,location.speed]];
 }
 
 - (UIView*)viewForZoomingInScrollView:(UIScrollView*)scrollView{ // 实现代理方法， step 3
@@ -167,6 +211,11 @@
         CPModalWebViewController *webC = [[CPModalWebViewController alloc] initWithAddress:params];
         
         [self presentViewController:webC animated:YES completion:^{}];
+        
+    } else if([method isEqualToString:@"getLocation"]){
+        _locationGettingCallback=callback;
+        [self startGettingLocation];
+        
         
     } else if ([method isEqualToString:@"getDeviceToken"]){
         NSString *key=@"DeviceToken";
